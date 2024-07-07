d3c83c821222cd79f06104233fb630b4bc4e9c18
ryann1203 <oxford1203@naver.com>
2024-07-08 00:18:16 +0900

    여름방학 세션 2주차

--- project/singerinfo/models.py
+++ project/singerinfo/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+def image_upload_path(instance, filename):
+    return f'{instance.pk}/{filename}'
+
+class Tag(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+
 # Create your models here.
 class Singer(models.Model):
     id = models.AutoField(primary_key=True)
@@ -8,6 +15,8 @@ class Singer(models.Model):
     debut = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    tag = models.ManyToManyField(Tag, blank=True)
+    image = models.ImageField(upload_to=image_upload_path, blank=True, null=True)
 
 class Song(models.Model):
     id = models.AutoField(primary_key=True)
--- project/singerinfo/urls.py
+++ project/singerinfo/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import *
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 app_name="singerinfo"
 urlpatterns = [
     path('', views.singer_list_create),
     path('<int:singer_id>', views.singer_detail_update_delete),
     path('<int:singer_id>/song', views.song_read_create),
-]
+    path('tags/<str:tag_name>', views.find_tag),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
